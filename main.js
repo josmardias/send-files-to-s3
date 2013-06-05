892ff6c3a05c796e47de62f7d25944f398de00d5
Josmar Dias <josmarnet@gmail.com>
2013-06-05 01:01:46 -0300

    fixes #1

--- main.js
+++ main.js
@@ -1,64 +1,40 @@
 #!/usr/bin/env node
 
-var aws = require('aws2js');
-var fs = require('fs');
-var path = require('path');
-var async = require('async');
+'use strict';
 
-var configFile = path.resolve(path.dirname(require.main.filename), 'config.json');
-var config = JSON.parse(fs.readFileSync(configFile, encoding="ascii"));
+var aws = require('aws2js'),
+    fs = require('fs'),
+    path = require('path'),
+    async = require('async');
 
-function chunk (arr, chunkSize) {
-    var retArr = [];
-    for (var i = 0; i < arr.length - (chunkSize - 1); i++){
-        retArr.push(arr.slice(i, i+chunkSize));
-    }
-    return retArr;
-}
+var configFile = path.resolve(path.dirname(require.main.filename), 'config.json'),
+    config = JSON.parse(fs.readFileSync(configFile, 'ascii'));
 
+var data = fs.readFileSync(config.files, 'ascii'),
+    fileList = data.split("\n").filter(function (e) { return e; }),
+    s3 = aws.load('s3', config.accessKey, config.secretKey);
 
-fs.readFile(config.files, 'ascii', function (err, data) {
-    if (err) throw err;
+console.log(fileList.length + ' total files');
+s3.setBucket(config.bucket);
 
-    var root = config.namespace;
-    var fileList = data.split("\n");
-    var files = [];
-    for (i in fileList) {
-        var filePath = fileList[i];
-        if(!filePath) break;
-        files.push({
-            path: filePath.split(root)[1],
-            file: filePath
-        })
-    }
+var iterator = function (item, callback) {
+    var s3Path = item.split(config.namespace)[1],
+        file = item,
+        headers = config.headers;
 
-    var akey, skey;
-    akey = config.accessKey;
-    skey = config.secretKey;
-
-    var s3 = aws.load('s3', akey, skey);
-    s3.setBucket(config.bucket);
-
-    var iterator = function (el, callback) {
-        var headers = config.headers;
-        s3.putFiles(el, '', headers, function (err, result) {
-            var errors = err.filter(function (e) {
-                return e ? e : false;
-            });
-            if(errors.length) {
-                callback(errors);
-                return;
-            }
-
-            callback(null);
-        });
-    };
-
-    chunckedFiles = chunk(files, 20);
-    async.eachSeries(chunckedFiles, iterator, function (err) {
-        if (err) console.log(err);
-        console.log('finished uploading');
+    s3.putFile(s3Path, file, '', headers, function (err, ret) {
+        //hack: maping files to errors
+        callback(null, err);
     });
+};
 
-})
+async.mapLimit(fileList, 10, iterator, function (err, ret) {
+    //hack: don't expect any errors, they come inside ret
+    if (err) { throw err; }
+
+    //hack: give me just the true errors
+    var errors = ret.filter(function (e) { return e; });
+
+    console.log(errors.length + ' errors');
+});
 
